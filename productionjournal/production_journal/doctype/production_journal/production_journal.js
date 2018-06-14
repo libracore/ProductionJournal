df48fb1e37ef6aed2f675f9c9caa4b0d5ef7435d
Joelios <joel.mesmer@libracore.com>
2018-06-14 17:19:56 +0200

    removed useless custom button

--- productionjournal/production_journal/doctype/production_journal/production_journal.js
+++ productionjournal/production_journal/doctype/production_journal/production_journal.js
@@ -3,20 +3,7 @@
 
 frappe.ui.form.on('Production Journal', {
 	refresh: function(frm) {
-		if (!frm.doc.__islocal) {
-			frm.add_custom_button(__("Reload Data"), function() {
-				frappe.call({
-				   method: "productionjournal.production_journal.doctype.production_journal.production_journal.clear_data",
-				   args: {
-						"name": frm.doc.name
-				   },
-				   callback: function(response) {
-						frappe.show_alert('Data reloaded', 5);
-						cur_frm.reload_doc();
-				   }
-				});
-			});
-		}
+
 	},
 	onload: function(frm) {
 		cur_frm.set_query("production_order", function() {
